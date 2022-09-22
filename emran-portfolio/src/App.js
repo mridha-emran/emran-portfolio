42f2860227b69d4b2e38ce1ce0e40126be8ee1f9
mridha-emran <bari.lala490@gmail.com>
2022-09-22 16:11:31 +0200

    navbar add

--- emran-portfolio/src/App.js
+++ emran-portfolio/src/App.js
@@ -1,14 +1,17 @@
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
 import About from './views/about/About'
 import Contact from './views/contact/Contact'
 import Home from './views/home/Home'
 import Skills from './views/skills/Skills'
 
 
+
 function App() {
   return (
     <>
       <Router>
+        <Navbar />
       <Routes>     
           <Route path="/" element={<Home/>} />
           <Route path="/about" element={<About />} />
---
+++ emran-portfolio/src/components/navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom"
+import { motion } from "framer-motion";
+import './navbar.scss'
+import Logo from "../../assest/Logo-em.png"
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <motion.div initial={{ y: "-100%" }} whileInView={{ y: 0 }}>
+        <img  className ="Logo" src={Logo} alt="portfolio" />
+      </motion.div>
+
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/skills">Skills</Link>
+        <Link to="/about">About</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
