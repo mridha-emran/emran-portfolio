b559b843be020754fc71ed08240c656d16933175
mridha-emran <bari.lala490@gmail.com>
2022-10-03 14:06:44 +0200

    home component test zone add

--- emran-portfolio/src/views/home/Home.jsx
+++ emran-portfolio/src/views/home/Home.jsx
@@ -1,12 +1,71 @@
+import { useEffect, useState } from 'react'
+import { motion} from "framer-motion";
 
-;
+import AnimatedLetters from '../../components/animatedLetters/AnimatedLetters';
+//scss
+import './home.scss';
+
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const [nameClass, setNameClass] = useState('text-animate')
+  const skillArray = 'Full stack'.split('')
+  const jobArray = 'Web Developer'.split('')
+
+   useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+      setNameClass('myName')
+    }, 4000)
+    return () => clearTimeout(timer)
+  }, [])
+
 
 
   return (
     <>
-  
-      <h1>home</h1>
+        <div className="container-main-home">
+       
+          <div className="container-text">
+            
+            <div className="text-zone">
+               <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i</span>
+                <br/>
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
+                <motion.div
+                  className={`${letterClass} _14`}
+                  style={{ display: "inline-block" }}
+                  animate={{ rotate: [0, 50, 0] }}
+                  transition={{ repeat: Infinity, duration: 1.4, repeatDelay: 0.7 }}
+                >
+                  👋
+                </motion.div>
+                <br/>
+                <h1 className={`${nameClass} _15`}>EMRAN </h1>
+                <AnimatedLetters
+                  letterClass={letterClass}
+                  strArray={skillArray}
+                  idx={22}
+                 />
+                <br/>
+                <AnimatedLetters
+                  letterClass={letterClass}
+                  strArray={jobArray}
+                  idx={22}
+                />
+               </h1>
+               <h2>Full Stack Developer/ Avid Learner</h2>
+            </div>
+          </div>
+
+          <div className="container-cube">
+           <h1>cube</h1>
+          </div>
+
+        </div>
+     
   
       
     </>
